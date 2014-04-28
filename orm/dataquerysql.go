1414a3273b1f94a529c1a7aafbf91ccfdc8e014f
Mark Stephens <mr.morphic@gmail.com>
2014-04-28 20:11:43 +1200

    moved remaining SQL references from orm. to dataquerysql.go

--- orm/dataquerysql.go
+++ orm/dataquerysql.go
@@ -1,11 +1,27 @@
 package orm
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 )
+
+// database is actually a connection pool. The pool is automatically managed, and works across go-routines.
+var database *sql.DB
+
+// Execute a SQL query, returning the resulting rows.
+func Query(sql string) (q *sql.Rows, e error) {
+	st, e := database.Prepare(sql)
+	if e != nil {
+		return
+	}
+
+	q, e = st.Query()
+	return
+}
 
 // DataQuerySQL is an implementer of DataQuery for SQL databases.
 type DataQuerySQL struct {
@@ -118,4 +134,48 @@ func NewQuerySQL(className string) DataQuery {
 	q.start = -1
 	q.baseClass = className
 	return q
+}
+
+func DataObjectFromRow(r *sql.Rows) (obj DataObject, e error) {
+	// Get columns
+	cols, e := r.Columns()
+	colCount := len(cols)
+
+	var field []interface{}
+	for i := 0; i < colCount; i++ {
+		switch {
+		case cols[i][:2] == "b:":
+			field = append(field, new(sql.NullBool))
+		case cols[i][:2] == "f:":
+			field = append(field, new(sql.NullFloat64))
+		case cols[i][:2] == "i:":
+			field = append(field, new(sql.NullInt64))
+		case cols[i][:2] == "s:":
+			field = append(field, new(sql.NullString))
+		case cols[i][:2] == "t:":
+			field = append(field, new(time.Time))
+		default:
+			field = append(field, new(sql.NullString))
+		}
+	}
+
+	//fmt.Printf("cols are %s\n", cols)
+	//fmt.Printf("there are %d columns\n", colCount)
+	//fmt.Println("about to scan values")
+	// get associated values
+	e = r.Scan(field...)
+
+	//fmt.Println("scanned fields")
+	if e != nil {
+		fmt.Printf("got an error though: %s\n", e)
+		return nil, e
+	}
+
+	m := NewDataObjectMap()
+
+	for i, c := range cols {
+		m[c] = field[i]
+	}
+
+	return m, nil
 }
--- orm/orm.go
+++ orm/orm.go
@@ -1,30 +1,13 @@
 package orm
 
 import (
-	"database/sql"
-	"fmt"
 	"github.com/mrmorphic/goss"
-	"time"
 )
-
-// database is actually a connection pool. The pool is automatically managed, and works across go-routines.
-var database *sql.DB
 
 // metadataSource is a path to the file containing metadata used by the ORM.
 var metadataSource string
 
 var dbMetadata *DBMetadata
-
-// Execute a SQL query, returning the resulting rows.
-func Query(sql string) (q *sql.Rows, e error) {
-	st, e := database.Prepare(sql)
-	if e != nil {
-		return
-	}
-
-	q, e = st.Query()
-	return
-}
 
 // DataObject is an interface for items returned by the ORM.
 type DataObject interface {
@@ -49,50 +32,6 @@ type DataQuery interface {
 	// Execute the query and return it's result. All error handling is returned via Run to
 	// simplify the signatures of chainable methods.
 	Run() (interface{}, error)
-}
-
-func DataObjectFromRow(r *sql.Rows) (obj DataObject, e error) {
-	// Get columns
-	cols, e := r.Columns()
-	colCount := len(cols)
-
-	var field []interface{}
-	for i := 0; i < colCount; i++ {
-		switch {
-		case cols[i][:2] == "b:":
-			field = append(field, new(sql.NullBool))
-		case cols[i][:2] == "f:":
-			field = append(field, new(sql.NullFloat64))
-		case cols[i][:2] == "i:":
-			field = append(field, new(sql.NullInt64))
-		case cols[i][:2] == "s:":
-			field = append(field, new(sql.NullString))
-		case cols[i][:2] == "t:":
-			field = append(field, new(time.Time))
-		default:
-			field = append(field, new(sql.NullString))
-		}
-	}
-
-	//fmt.Printf("cols are %s\n", cols)
-	//fmt.Printf("there are %d columns\n", colCount)
-	//fmt.Println("about to scan values")
-	// get associated values
-	e = r.Scan(field...)
-
-	//fmt.Println("scanned fields")
-	if e != nil {
-		fmt.Printf("got an error though: %s\n", e)
-		return nil, e
-	}
-
-	m := NewDataObjectMap()
-
-	for i, c := range cols {
-		m[c] = field[i]
-	}
-
-	return m, nil
 }
 
 func IsHierarchical(className string) bool {
