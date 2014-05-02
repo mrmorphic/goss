a531dc553c8120d5e2498205f7cc5818eace94ba
Mark Stephens <mr.morphic@gmail.com>
2014-05-03 11:31:16 +1200

    support for registering models

--- orm/orm.go
+++ orm/orm.go
@@ -2,12 +2,17 @@ package orm
 
 import (
 	"github.com/mrmorphic/goss"
+	"reflect"
 )
 
 // metadataSource is a path to the file containing metadata used by the ORM.
 var metadataSource string
 
 var dbMetadata *DBMetadata
+
+// A map of class names to DataObject instances which is used when we get objects
+// from the database.
+var models map[string]DataObject
 
 // DataObject is an interface for items returned by the ORM.
 type DataObject interface {
@@ -36,4 +41,33 @@ type DataQuery interface {
 
 func IsHierarchical(className string) bool {
 	return dbMetadata.IsHierarchical(className)
+}
+
+// Register one or more model instances. The map key is the ClassName value returned in
+// a data object fetch, and the instance is an object that will be used as a prototype
+// for generating new DataObject instances.
+func RegisterModels(m map[string]DataObject) {
+	// @todo make concurrency-safe.
+	for k, v := range m {
+		models[k] = v
+	}
+}
+
+// Given a class name, return a DataObject instance. If className has been registered
+// using RegisterModels, then a new, empty instance of the data object concrete type
+// is returned. Otherwise, a DataObjectMap is returned.
+func GetModelInstance(className string) DataObject {
+	proto := models[className]
+	if proto == nil {
+		return NewDataObjectMap()
+	}
+
+	// Get the type that the interface points to
+	t := reflect.TypeOf(proto)
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	new := reflect.New(t)
+	return new.Interface().(DataObject)
 }
